5fa778709a20eeed4f406ad3172977ecabcb400c
Letladi Sebesho <sletladi@gmail.com>
2018-05-24 21:05:35 +0200

    MALIK, ch7, exercise 8

--- malik-js/ch7/stack.js
+++ malik-js/ch7/stack.js
@@ -1,27 +1,41 @@
-const LinkedList = require('../ch5/LinkedList')
+class Node {
+    constructor(val = null) {
+        this.info = val
+        this.link = null
+    }
+}
 
 class Stack {
     constructor() {
-        this.list = new LinkedList
+        this.front = null
     }
 
     push(val) {
-        this.list.insertFirst(val)
-    }
-
-    pop() {
-        const val = this.top()
-        if (val) this.list.deleteAt(0)
-        return val
+        const node = new Node(val)
+        if (this.isEmpty()) {
+            this.front = node
+        } else {
+            node.link = this.front
+            this.front = node
+        }
     }
 
     top() {
-        return this.isEmpty() ? null : this.list.front
+        if (this.isEmpty()) return null
+        return this.front.info
+    }
+
+    pop() {
+        if (this.isEmpty()) return null
+        const val = this.front.info
+        this.front = this.front.link
+        return val
     }
 
     isEmpty() {
-        return this.list.isEmpty()
+        return this.front === null
     }
 }
+
 
 module.exports = Stack
