1416b350670ff5b5805376666494a3718b241303
Letladi Sebesho <sletladi@gmail.com>
2018-06-11 16:47:06 +0200

    interval insertion sort

--- malik-js/ch10/insertionSort.js
+++ malik-js/ch10/insertionSort.js
@@ -1,11 +1,19 @@
-const insertionSort = (list) => {
-    for (let i = 1, iBefore = 0, len = list.length; i < len; i++, iBefore++) {
+const insertionSort = (list, from = 0, to) => {
+    if (from > to) throw new Error("An interval's opening value cannot be greater than its closing value")
+
+    from = (from < 0) ? 0 : from
+    const len = list.length
+    to = ((to > len - 1) || (to === void(0))) ? (len - 1) : to
+
+    for (let i = from + 1, iBefore = from; i <= to; i++, iBefore++) {
         if (list[i] < list[iBefore]) {
-            let j = i, jBefore = iBefore, temp = list[i]
+            let j = i
+              , jBefore = iBefore
+              , temp = list[i]
             do {
                 list[j] = list[jBefore]
                 j = jBefore--
-            } while (j > 0 && list[jBefore] > temp)
+            } while (j > from && list[jBefore] > temp)
             list[j] = temp
         }
     }
--- malik-js/ch10/insertionSort.spec.js
+++ malik-js/ch10/insertionSort.spec.js
@@ -16,4 +16,36 @@ describe('insertionSort', () => {
         sort(list)
         expect(list).toEqual([10, 17, 18, 23, 25, 30, 35, 45])
     })
+
+    describe('intervalInsertionSort', () => {
+      test('sort([34, 67, 23, 12, 78, 56, 36, 79, 5, 32, 66], 2, 7) = [34, 67, 12, 23, 36, 56, 78, 79, 5, 32, 66', () => {
+          const list = [34, 67, 23, 12, 78, 56, 36, 79, 5, 32, 66]
+          sort(list, 2, 7)
+          expect(list).toEqual([34, 67, 12, 23, 36, 56, 78, 79, 5, 32, 66])
+      })
+      test('sort([13, 7, 15, 8, 12, 30, 3, 20], 3) = [3, 7, 8, 12, 13, 15, 20, 30]', () => {
+          const list = [13, 7, 15, 8, 12, 30, 3, 20]
+          sort(list, 3)
+          expect(list).toEqual([13, 7, 15, 3, 8, 12, 20, 30])
+      })
+      test('sort([10, 18, 25, 30, 23, 17, 45, 35], 0) = [10, 17, 18, 23, 25, 30, 35, 45]', () => {
+          const list = [10, 18, 25, 30, 23, 17, 45, 35]
+          sort(list, 0)
+          expect(list).toEqual([10, 17, 18, 23, 25, 30, 35, 45])
+      })
+      test('sort([10, 18, 25, 30, 23, 17, 45, 35], 5, 2) = 🙅‍ (interval must be valid)', () => {
+          const list = [10, 18, 25, 30, 23, 17, 45, 35]
+          expect(() => sort(list, 5, 2)).toThrow()
+      })
+      test('sort([10, 18, 25, 30, 23, 17, 45, 35], -5) = [10, 17, 18, 23, 25, 30, 35, 45] (negative opening value goes to beginning of the list)', () => {
+        const list = [10, 18, 25, 30, 23, 17, 45, 35]
+        sort(list, -5)
+        expect(list).toEqual([10, 17, 18, 23, 25, 30, 35, 45])
+      })
+      test('sort([13, 7, 15, 8, 12, 30, 3, 20], 3, 20) = [3, 7, 8, 12, 13, 15, 20, 30]  (values greater than the length of the list go to the end of the list)', () => {
+          const list = [13, 7, 15, 8, 12, 30, 3, 20]
+          sort(list, 3, 20)
+          expect(list).toEqual([13, 7, 15, 3, 8, 12, 20, 30])
+      })
+    })
 })
