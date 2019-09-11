8be64e98782b9fa7345509b054c89eb3a9459693
Letladi <sletladi@gmail.com>
2019-09-11 17:18:36 +0200

    ex8.9 (art-of-scheme)

--- art-of-scheme/ch8/sec8-3-ex.scm
+++ art-of-scheme/ch8/sec8-3-ex.scm
@@ -50,43 +50,6 @@
     )
 )
 
-; (define intersection
-;     (lambda args
-;         (cond
-;             ((null? args) the-empty-set)
-;             ((= (length args) 1) args)
-;             (else
-;                 (letrec*
-;
-;                     (
-;                         (s1 (car args))
-;                         (s2 (cadr args))
-;                         (rest (cddr args))
-;                         (helper (lambda (s1)
-;                             (if (empty-set? s1)
-;                                 the-empty-set
-;                                 (let ((elem (pick s1)))
-;                                     (if ((contains s2) elem)
-;                                         (adjoin elem (helper ((residue elem) s1)))
-;                                         (helper ((residue elem) s1))
-;                                     )
-;                                 )
-;                             )
-;                         ))
-;                         (intersect-two (helper s1))
-;                     )
-;
-;
-;                     (apply
-;                         intersection
-;                         (cons intersect-two rest)
-;                     )
-;                 )
-;             )
-;         )
-;     )
-; )
-
 (define intersection (set-fn-builder
     (lambda (set1 set2)
         the-empty-set
@@ -96,42 +59,6 @@
     )
 ))
 
-; (define union
-;     (lambda args
-;         (cond
-;             ((null? args) the-empty-set)
-;             ((= (length args) 1) args)
-;             (else
-;                 (letrec*
-;                     (
-;                         (s1 (car args))
-;                         (s2 (cadr args))
-;                         (rest (cddr args))
-;                         (helper (lambda (s1)
-;                             (if (empty-set? s1)
-;                                 s2
-;                                 (let ((elem (pick s1)))
-;                                     (if (not ((contains s2) elem))
-;                                         (adjoin elem (helper ((residue elem) s1)))
-;                                         (helper ((residue elem) s1))
-;                                     )
-;                                 )
-;                             )
-;                         ))
-;                         (union-two (helper s1))
-;                     )
-;
-;                     (apply
-;                         union
-;                         (cons union-two rest)
-;                     )
-;                 )
-;             )
-;         )
-;
-;     )
-; )
-
 (define union (set-fn-builder
     (lambda (set1 set2)
         set2
@@ -140,3 +67,30 @@
         (not ((contains set2) elem-of-first-set))
     )
 ))
+
+(define symmetric-difference
+    (lambda (s1 s2)
+        (if (empty-set? s1)
+            s2
+            (let*
+                (
+                    (elem (pick s1))
+                    (residue-elem (residue elem))
+                )
+                (if ((contains s2) elem)
+                    (symmetric-difference
+                        (residue-elem s1)
+                        (residue-elem s2)
+                    )
+                    (adjoin
+                        elem
+                        (symmetric-difference
+                            (residue-elem s1)
+                            s2
+                        )
+                    )
+                )
+            )
+        )
+    )
+)
