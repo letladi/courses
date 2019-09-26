bb48ad0894e838bd552711b1963232e91890c38a
Letladi <sletladi@gmail.com>
2019-09-26 17:26:07 +0200

    working on last ex of ch9 (art-of-scheme)

--- art-of-scheme/ch9/array-of-array.scm
+++ art-of-scheme/ch9/array-of-array.scm
@@ -1,3 +1,12 @@
+; we note that (m1xm2xm3) is the size of all the elements in the array of arrays of arrays
+; to get array i, we times i by m1 to get index i, to get array j we add i to the product of jxm2 to get p
+; and then to get k, we add p to the product of kxm3.
+
+; In general, to get the index i in the vector for any array of arrays with dimension M1xM2...xMn, given indices i1, i2, i3,..., in,
+; we perfom the following computation; i = i1xM1 + ... + inxMn
+
+; On the other hand, to perform the computation the other way around; that given an index i the vector, how do we get the i,j,k
+; in the array of arrays of arrays
 (define size-dim-1
     (lambda (arr)
         (let
@@ -30,5 +39,11 @@
             )
             (quotient m2-times-m3 m2)
         )
+    )
+)
+
+(define array-ref
+    (lambda (arr)
+
     )
 )
