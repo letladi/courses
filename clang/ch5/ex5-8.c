43b2bb8bab7f7413dac2dca82e85cd9210d06749
Letladi <sletladi@gmail.com>
2020-05-22 07:56:05 +0200

    done with ex5-8 (K&R)

--- clang/ch5/ex5-8.c
+++ clang/ch5/ex5-8.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include isLeap(x) ((x) = (x) % 4 == 0 && (x) % 100 != 0 || (x) % 400 == 0)
+#include valid_year(x) ((x) > 0)
+#include valid_month(month) (1 <= (month) && (month) <= 12)
+#include valid_day(d) (1 <= (d) && (d) <= 366)
+
 static char daytab[2][13] = {
    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
@@ -6,8 +13,16 @@ static char daytab[2][13] = {
 //set day of year from month and day
 int day_of_year(int year, int month, int day)
 {
+   bool hasError = false;
+   if (hasError = !valid_year(year))
+      printf("error (day_of_year): year must be a positive number\n");
+   if (hasError = hasError || !valid_month(month))
+      printf("error (day_of_year): month must be between 1 and 12");
+
+   if (hasError) return -1;
+
    int i, leap;
-   leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+   leap = isLeap(year);
    for (i = 1; i < month; i++) day += daytab[leap][i];
    return day;
 }
@@ -15,8 +30,20 @@ int day_of_year(int year, int month, int day)
 // set month, day from day of year
 void month_day(int year, int yearday, int *pmonth, int *pday)
 {
+   bool hasError = false;
+   if (hasError = !valid_year(year))
+      printf("error (month_day): year must be a positive number\n");
+   if (hasError = hasError || !valid_month(month))
+      printf("error (month_day): month must be between 1 and 12");
+   if (hasError = hasError || !valid_day(yearday))
+      printf("error (month_day): day must be positive and less than 366");
+   if (hasError) {
+      *pday = -1;
+      pmonth = -1;
+      return;
+   }
    int i, leap;
-   leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+   leap = isLeap(year);
    for (i = 1; yearday > daytab[leap][i]; i++) yearday -= daytab[leap][i];
 
    *pmonth = i;
