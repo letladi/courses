b38f03a48d9105f50e707066804ca01edef706c2
Letladi <sletladi@gmail.com>
2020-07-28 22:17:54 +0200

    done with prog-2c, knking, ch13

---
+++ knking/ch13/exercises/prog-2c.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX_REMIND 50
+#define MONTHS_IN_YR 12
+#define MONTH_NAME_LEN 3
+#define MIN_IN_HR 60
+#define MSG_LEN 60
+#define TIME_LEN 5
+
+const char months[MONTHS_IN_YR][3] = {
+   "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+};
+
+int read_line(char str[], int n);
+int get_minutes_from_midnight(char str[]);
+int timecmp(int t1, int t2);
+int daycmp(char *, char *);
+int monthcmp(char*, char*);
+
+int main(void)
+{
+   char reminders[MAX_REMIND][MSG_LEN + TIME_LEN + 3];
+   char day_str[MONTH_NAME_LEN + 4], msg_str[MSG_LEN + 1];
+   int day, month, i, j, hr, min, time, num_remind = 0;
+
+   for ( ; ; ) {
+      if (num_remind == MAX_REMIND) {
+         printf("-- No space left -- \n");
+         break;
+      }
+
+      printf("Enter month/day, 24-hour time and reminder: ");
+      scanf("%2d/%2d", &month, &day);
+
+      if (day == 0 || month == 0) break;
+
+      if (day < 0 || day > 31) {
+         printf("day value must be between 1 and 31, you entered %d. Please try again...\n", day);
+         continue;
+      }
+
+      if (month <= 0 || month > 12) {
+         printf("month value must be between 1 and 12, you entered %d. Please try again...\n", month);
+         continue;
+      }
+
+      scanf(" %2d:%2d", &hr, &min);      
+
+      sprintf(day_str, "%.2d/%.2d", month, day);
+      sprintf(msg_str, " %.2d:%.2d  ", hr, min);
+      read_line(msg_str + TIME_LEN + 1, MSG_LEN);
+
+      time = hr * MIN_IN_HR + min;
+      int day_cmp, time_cmp, month_cmp;
+      for (i = 0; i < num_remind; i++) {
+         
+         month_cmp = monthcmp(day_str, reminders[i]);
+         if (month_cmp < 0) break;
+         else if (month_cmp == 0) {
+            while ((day_cmp = daycmp(day_str, reminders[i])) > 0) i++;
+            if (day_cmp < 0) break;
+            else if (day_cmp == 0) {
+               time_cmp = timecmp(time, get_minutes_from_midnight(reminders[i]));
+               while (
+                  i < num_remind &&
+                  (day_cmp == 0) &&
+                  (time_cmp = timecmp(time, get_minutes_from_midnight(reminders[i]))) > 0
+               ) i++;
+               break;
+            }
+         }
+          
+      }
+        
+      for (j = num_remind; j > i; j--)
+         strcpy(reminders[j], reminders[j - 1]);
+
+      strcpy(reminders[i], day_str);
+      strcat(reminders[i], msg_str);
+
+      num_remind++;
+   }
+
+   printf("\nDay Reminder\n");
+   for (i = 0; i < num_remind; i++) printf(" %s\n", reminders[i]);
+
+   return 0;
+}
+
+int read_line(char str[], int n)
+{
+   int ch, i = 0;
+   while ((ch = getchar()) != '\n')
+      if (i < n) str[i++] = ch;
+   str[i] = '\0';
+   return i;
+}
+
+int get_minutes_from_midnight(char str[])
+{
+   int time = 0, hr = 0, min = 0;
+   int i = 0;
+   while (str[i] != ':') i++;
+
+   int j = i - 1;
+   hr += str[j--] - '0';
+   hr += (str[j] - '0') * 10;
+   time += hr * MIN_IN_HR;
+   j = i + 2;
+   min += str[j--] - '0';
+   min += (str[j] - '0') * 10;
+   time += min;
+
+   return time;
+}
+
+int get_day(char str[])
+{
+   int i = 0, ret = 0;
+   while (str[i] != '/') i++;
+
+   if (str[i] == '\0') return -1;
+   else i++;
+
+   if (isdigit(str[i + 1])) {
+      ret += (str[i] - '0') * 10;
+      i++; 
+   }
+
+   ret += str[i] - '0';
+   return ret;
+}
+
+int daycmp(char t1[], char t2[])
+{
+   return get_day(t1) - get_day(t2);
+}
+
+int timecmp(int t1, int t2)
+{
+   return t1 - t2;
+}
+
+int get_month(char *t)
+{  
+   int i = 0, ret = 0;
+   while (!isdigit(t[i])) i++;
+   ret += (t[i++] - '0') * 10;
+   ret += t[i] - '0';
+   return ret;
+}
+
+int monthcmp(char* t1, char* t2)
+{
+   return get_month(t1) - get_month(t2);
+}
