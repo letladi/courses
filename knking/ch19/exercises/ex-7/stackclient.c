af9fb10953b15b2b14641938e14b3472094f8377
Letladi Sebesho <sletladi@gmail.com>
2021-04-22 00:51:46 +0200

    ex-7, ch19, knking

---
+++ knking/ch19/exercises/ex-7/stackclient.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <assert.h>
+#include "stackADT.h"
+
+int main(void)
+{
+   Stack s1, s2;
+
+   s1 = create(5);
+   s2 = create(10);
+
+   assert(s1 != s2);
+
+   push(s1, 1);
+   assert(peek(s1) == 1);
+   push(s1, 2);
+   assert(peek(s1) == 2);
+
+   assert(is_empty(s2));
+   assert(!is_empty(s1));
+
+   assert(pop(s1) == 2);
+   push(s2, 2);
+   assert(pop(s1) == 1);
+
+   push(s2, 1);
+   assert(peek(s2) == 1);
+   destroy(s1);
+
+   while (!is_empty(s2)) pop(s2);
+
+   assert(is_empty(s2));
+
+   push(s2, 3);
+   assert(peek(s2) == 3);
+   make_empty(s2);
+   assert(is_empty(s2));
+
+   Item test_list[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+   for (int i = 0; i < 10; i++) {
+      push(s2, test_list[i]);
+   }
+
+   printf("Stack works....\n");
+
+   return 0;
+}
